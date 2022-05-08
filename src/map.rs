b569cab84c367577d0e09eb1972c575dcfe46186
taochen <taochen@yunjumai.com>
2022-05-08 22:47:10 +0800

    add_map_macro

---
+++ src/map.rs
@@ -0,0 +1,67 @@
+#[macro_export]
+macro_rules! hashmap {
+    (@single $($x:tt)*) => (());
+    (@count $($key_val:expr),*) => (<[()]>::len(&[$(hashmap!(@single $key_val)),*]));
+
+    ($($key:expr => $value:expr,)+) => { hashmap!($($key => $value),+) };
+    ($($key:expr => $value:expr),*) => {
+        {
+            let _cap = hashmap!(@count $($key),*);
+            let mut _map = ::std::collections::HashMap::with_capacity(_cap);
+            $(
+                let _ = _map.insert($key, $value);
+            )*
+            _map
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! hashset {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(hashset!(@single $rest)),*]));
+
+    ($($key:expr,)+) => { hashset!($($key),+) };
+    ($($key:expr),*) => {
+        {
+            let _cap = hashset!(@count $($key),*);
+            let mut _set = ::std::collections::HashSet::with_capacity(_cap);
+            $(
+                let _ = _set.insert($key);
+            )*
+            _set
+        }
+    };
+}
+
+
+#[macro_export]
+macro_rules! btreemap {
+    // trailing comma case
+    ($($key:expr => $value:expr,)+) => (btreemap!($($key => $value),+));
+
+    ( $($key:expr => $value:expr),* ) => {
+        {
+            let mut _map = ::std::collections::BTreeMap::new();
+            $(
+                let _ = _map.insert($key, $value);
+            )*
+            _map
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! btreeset {
+    ($($key:expr,)+) => (btreeset!($($key),+));
+
+    ( $($key:expr),* ) => {
+        {
+            let mut _set = ::std::collections::BTreeSet::new();
+            $(
+                _set.insert($key);
+            )*
+            _set
+        }
+    };
+}
