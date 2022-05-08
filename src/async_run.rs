715428249254b3d9ff041d05b5dfafc2b17ab503
taochen <taochen@yunjumai.com>
2022-05-08 22:58:33 +0800

    add_trace_async_fn

--- src/async_run.rs
+++ src/async_run.rs
@@ -13,3 +13,19 @@ macro_rules! async_fn {
         }
     };
 }
+
+#[macro_export]
+macro_rules! trace_async_fn {
+    ($trace_id: expr, $async_fn:expr) => {
+        match $async_fn.await {
+            Ok(value) => {
+                tracing::info!("trace_id: {} result: {:?}", $trace_id, value);
+                Ok(value)
+            }
+            Err(err) => {
+                tracing::error!("trace_id: {} error: {:?}", $trace_id, err);
+                Err(err)
+            }
+        }
+    };
+}
