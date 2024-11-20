bd515d5c908cedb8bbb5d1f56b14ba520493f8f1
taochen <@>
2024-11-20 14:47:09 +0800

    update

--- src/if_panic.rs
+++ src/if_panic.rs
@@ -1,24 +1,31 @@
 #[macro_export]
 macro_rules! if_panic {
     ($func:expr) => {
-        match std::panic::catch_unwind(|| $func) {
-            Ok(result) => result,
-            Err(e) => {
-                #[cfg(feature = "tracing")]
-                tracing::error!("panic_info: {:?}", e.downcast_ref::<&str>());
-                Default::default()
-            }
-        }
+        $func
     };
 
     ($func:expr, $err_val: expr) => {
-        match std::panic::catch_unwind(|| $func) {
-            Ok(result) => result,
-            Err(e) => {
-                #[cfg(feature = "tracing")]
-                tracing::error!("panic_info: {:?}", e.downcast_ref::<&str>());
-                $err_val
-            }
-        }
+        $func
     };
+    // ($func:expr) => {
+    //     match std::panic::catch_unwind(|| $func) {
+    //         Ok(result) => result,
+    //         Err(e) => {
+    //             #[cfg(feature = "tracing")]
+    //             tracing::error!("panic_info: {:?}", e.downcast_ref::<&str>());
+    //             Default::default()
+    //         }
+    //     }
+    // };
+    //
+    // ($func:expr, $err_val: expr) => {
+    //     match std::panic::catch_unwind(|| $func) {
+    //         Ok(result) => result,
+    //         Err(e) => {
+    //             #[cfg(feature = "tracing")]
+    //             tracing::error!("panic_info: {:?}", e.downcast_ref::<&str>());
+    //             $err_val
+    //         }
+    //     }
+    // };
 }
